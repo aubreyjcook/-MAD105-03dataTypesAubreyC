350315da6bdbd0ef87610f0aecb9670938c26353
Aubrey Cook <aubreyjcook@gmail.com>
2016-09-10 18:40:52 -0500

    complete

---
+++ DataTypes/initializevariables/src/main/java/com/example/MyClass.java
@@ -0,0 +1,36 @@
+package com.example;
+
+/* Program by Aubrey Cook
+*
+* This program is meant to demonstrate all the PRIMITIVE data types in Java and initialize them.
+* */
+
+public class MyClass
+{
+    public static void main(String[] args){
+
+        byte byteExample = 0;
+        short shortExample = 14;
+        int intExample = 100000;
+        long longExample = 50L;
+
+        float floatExample = 0.99999f;
+        double doubleExample = 0.12d;
+
+        boolean boolExample = false;
+
+        char charExample = 's';
+
+        System.out.println(byteExample);
+        System.out.println(shortExample);
+        System.out.println(intExample);
+        System.out.println(longExample);
+
+        System.out.println(floatExample);
+        System.out.println(doubleExample);
+
+        System.out.println(boolExample);
+
+        System.out.println(charExample);
+    }
+}
